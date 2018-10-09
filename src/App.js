394718ef26163c4cf755bba2363566690b7e8872
Kenny Yang <kennyyang19@gmail.com>
2018-10-09 10:30:28 -0500

    done

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -33,9 +33,13 @@ class App extends Component {
   }
 }
 
+// const mapStateToProps = (state) => {
+//   debugger;
+//   return { items: state.items }
+// }
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  return { orangePeel: state.items }
 }
-
+ 
+// export default connect(vanilla)(App);
 export default connect(mapStateToProps)(App);
